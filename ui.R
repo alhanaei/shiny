3e526697a86aa791aedfa152034a9cf6d1c14d44
alhanaei@hotmail.com <alhanaei@hotmail.com>
2015-05-14 15:27:05 +0400

    update 2

--- ui.R
+++ ui.R
@@ -1,13 +1,13 @@
 library(shiny)
 
-# Define UI for miles per gallon application
+# Define UI for miles per gallon vs variable application
 shinyUI(pageWithSidebar(
     
     # Application title
     headerPanel("Miles Per Gallon and Transmission"),
     
     # Sidebar with controls to select the variable to plot against mpg
-    # and to specify whether outliers should be included
+
     sidebarPanel(
         selectInput("variable", "Variable:",
                     list("Gross horsepower" = "hp", 
